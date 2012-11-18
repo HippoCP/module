852185a79c965b25f53dc342dc44b027f603065b
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-11-18 23:38:26 +0000

    Start working on hippocp.model.user

--- src/models/user.py
+++ src/models/user.py
@@ -7,34 +7,16 @@ from pwd import getpwnam
 from hippocp import database
 
 class User:
-    """Class to manage the users.
+    """User model the users."""
 
-    Database schema:
-        uid     --- UNIX User ID
-        name    --- Nice user name (max 20 chars)
-        package --- Package ID
-        father  --- User's reseller/admin
-    """
-    def __init__(self):
-        """Initialize the Packages class"""
+    def __init__(self, uid):
+        """Initialize the User model"""
         self.db = postgresql.open(database.getPgUrl())
+        self.id = uid
 
-    def add(self, name, package, father, password):
-        """Add a user"""
-        encPass = crypt.crypt(password,"22")
-        os.system("useradd -p " + encPass + " " + name)
-        uid = str(getpwnam(name).pw_uid)
-        if self.db.execute('INSERT INTO users (uid, name, package, father) VALUES (\''+uid+'\', \''+name+'\', \''+package+'\', \''+father+'\')') is None:
-            return True
-        else:
-            return False
-
-    def list(self, father = None):
-        """List users.
-        If no arguments are passed, all users on the system will be showed
-        otherwise only that user childs will be passed."""
-        sql = "SELECT * FROM users"
-        if father:
-            sql += " WHERE father = " + father
+    def roles(self):
+        """List all roles of the user"""
+        sql = "SELECT * FROM roles"
+        sql+= "    JOIN role_user ON role_user.user_id = " + self.id
         rows = self.db.prepare(sql)
         return rows()
