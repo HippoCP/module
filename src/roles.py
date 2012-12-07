6e973d405e51416663a3d0244fb2ef63f9594d0f
Fabio Alessandro Locati <fabiolocati@gmail.com>
2012-12-07 12:03:47 +0000

    Add hippocp.roles

---
+++ src/roles.py
@@ -0,0 +1,16 @@
+import postgresql
+
+from hippocp import database
+
+class Roles:
+    """Class to manage roles."""
+    def __init__(self):
+        """Initialize the Roles class"""
+        self.db = postgresql.open(database.getPgUrl())
+
+    def add(self, name):
+        """Add a role"""
+        if self.db.execute('INSERT INTO roles (name) VALUES (\''+name+'\')') is None:
+            return True
+        else:
+            return False
